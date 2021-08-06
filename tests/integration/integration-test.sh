14dc597f4893fcbb1460ff3bcf8fc1d2d90130dd
Teetje Stark <teetje.stark@gmx.de>
2021-08-06 11:39:35 +0200

    Remove alert from IT

--- tests/integration/integration-test.sh
+++ tests/integration/integration-test.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 set -euo pipefail
 
-# This script is expected to be called from the root folder of Connaisseur
-declare -i NUMBER_OF_VALID_DEPLOYMENTS=0
-declare -i NUMBER_OF_INVALID_DEPLOYMENTS=0
-
 echo 'Preparing Connaisseur config...'
 envsubst < tests/integration/update.yaml > update
 yq eval-all --inplace 'select(fileIndex == 0) * select(fileIndex == 1)' helm/values.yaml update
@@ -18,7 +14,6 @@ echo 'Successfully installed Connaisseur'
 
 echo 'Testing unsigned image...'
 kubectl run pod --image=securesystemsengineering/testimage:unsigned >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
 
 if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
   echo 'Failed to deny unsigned image or failed with unexpected error. Output:'
@@ -30,7 +25,6 @@ fi
 
 echo 'Testing image signed under different key...'
 kubectl run pod --image=library/redis >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
 
 if [[ ! "$(cat output.log)" =~ 'Failed to verify signature of trust data root.' ]]; then
   echo 'Failed to deny image signed with different key or failed with unexpected error. Output:'
@@ -42,7 +36,6 @@ fi
 
 echo 'Testing signed image...'
 kubectl run pod --image=securesystemsengineering/testimage:signed -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
 
 if [[ "$(cat output.log)" != 'pod/pod created' ]]; then
   echo 'Failed to allow signed image. Output:'
@@ -63,7 +56,6 @@ fi
 
 echo 'Testing signed image with designated signer...'
 kubectl run pod2 --image=securesystemsengineering/testimage:special_sig -lapp.kubernetes.io/instance=connaisseur >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
 
 if [[ "$(cat output.log)" != 'pod/pod2 created' ]]; then
   echo 'Failed to allow image signed by designated signer. Output:'
@@ -75,7 +67,6 @@ fi
 
 echo 'Testing image with missing designated signer...'
 kubectl run pod3 --image=securesystemsengineering/testimage:wrong_signer >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
 
 if [[ ! "$(cat output.log)" =~ 'Not all required delegations have trust data for image docker.io/securesystemsengineering/testimage:wrong_signer.' ]]; then
   echo 'Failed to deny image with missing designated signer or failed with unexpected error. Output:'
@@ -87,7 +78,6 @@ fi
 
 echo 'Testing image with differing designated signers...'
 kubectl run pod3 --image=securesystemsengineering/testimage:double_sig >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
 
 if [[ ! "$(cat output.log)" =~ 'Found multiple signed digests for image docker.io/securesystemsengineering/testimage:double_sig.' ]]; then
   echo 'Failed to deny image with missing designated signer or failed with unexpected error. Output:'
@@ -99,7 +89,6 @@ fi
 
 echo 'Testing deployment of unsigned init container along with a valid container...'
 kubectl apply -f tests/integration/valid_container_with_unsigned_init_container_image.yml >output.log 2>&1 || true
-NUMBER_OF_INVALID_DEPLOYMENTS+=1
 
 if [[ ! "$(cat output.log)" =~ 'Unable to find signed digest for image docker.io/securesystemsengineering/testimage:unsigned.' ]]; then
   echo 'Allowed an unsigned image via init container or failed due to an unexpected error handling init containers. Output:'
@@ -111,7 +100,6 @@ fi
 
 echo 'Testing deployment of valid init container along with a valid container...'
 kubectl apply -f tests/integration/valid_init_container.yaml >output.log 2>&1 || true
-NUMBER_OF_VALID_DEPLOYMENTS+=1
 
 if [[ "$(cat output.log)" != 'pod/connaisseur-integration-test-pod-valid-init created' ]]; then
   echo 'Failed to deploy a valid initContainer along with a valid container. Output:'
@@ -119,27 +107,6 @@ if [[ "$(cat output.log)" != 'pod/connaisseur-integration-test-pod-valid-init cr
   exit 1
 else
   echo 'Successfully allowed valid image in init container and container'
-fi
-
-echo 'Checking whether alert endpoints have been called successfully'
-ENDPOINT_HITS=$(curl ${ALERTING_ENDPOINT_IP}:56243 --header "Content-Type: application/json")
-let NUMBER_OF_DEPLOYMENTS=${NUMBER_OF_INVALID_DEPLOYMENTS}+${NUMBER_OF_VALID_DEPLOYMENTS}
-EXPECTED_ENDPOINT_HITS=$(jq -n \
---argjson REQUESTS_TO_SLACK_ENDPOINT ${NUMBER_OF_DEPLOYMENTS} \
---argjson REQUESTS_TO_OPSGENIE_ENDPOINT  ${NUMBER_OF_VALID_DEPLOYMENTS} \
---argjson REQUESTS_TO_KEYBASE_ENDPOINT ${NUMBER_OF_INVALID_DEPLOYMENTS} \
-'{
-"successful_requests_to_slack_endpoint":$REQUESTS_TO_SLACK_ENDPOINT,
-"successful_requests_to_opsgenie_endpoint": $REQUESTS_TO_OPSGENIE_ENDPOINT,
-"successful_requests_to_keybase_endpoint": $REQUESTS_TO_KEYBASE_ENDPOINT
-}')
-echo "Hit the alerting endpoints ${ENDPOINT_HITS} times; expected was ${EXPECTED_ENDPOINT_HITS}."
-diff <(echo $ENDPOINT_HITS | jq -S .) <(echo $EXPECTED_ENDPOINT_HITS | jq -S .) >output.log 2>&1
-if [[ -s output.log ]]; then
-  cat output.log
-  exit 1
-else
-  echo 'Successfully called mocked alert endpoints'
 fi
 
 echo 'Uninstalling Connaisseur...'
